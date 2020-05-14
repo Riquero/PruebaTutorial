5bce9834b08bb7e474f2ecb103c04de86c0baa6d
Riquero <Santiago.Riquero@gmail.com>
2020-05-14 13:17:11 +0200

    jpotuasd

--- ajedrezSRM/src/tablero/UtilidadesTablero.java
+++ ajedrezSRM/src/tablero/UtilidadesTablero.java
@@ -13,8 +13,7 @@ public class UtilidadesTablero {
 	public static final int NUMERO_DE_CASILLAS = 64;
 	public static final int NUMERO_DE_CASILLAS_POR_FILA = 8; 
 	
-	//Hola Jesús
-	// heyyyyy
+	// Hola Jesús
 
 	private UtilidadesTablero() {
 		throw new RuntimeException("No puedes instanciarme");
